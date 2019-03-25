a1533505a95496c50ed9fbf5d7371d6b8c9b8b28
Chan Hong Wing <jason021075@me.com>
2019-03-25 12:28:02 +0800

    Initial Commit

---
+++ try.Reachability/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  try.Reachability
+//
+//  Created by Chan Hong Wing on 25/3/2019.
+//  Copyright © 2019 Chan Hong Wing. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
